b0a2e4bf24eae6e00030ebe5b7dd767c4ea5746b
Femrock <mf.lennon@gmail.com>
2020-03-04 09:52:38 -0300

    fuchsia bottom line problem, fixed.

--- src/assets/components/SocialIcons.js
+++ src/assets/components/SocialIcons.js
@@ -7,19 +7,22 @@ export default function SocialIcons(){
   return (
     <div className="socialContainer">
       <a
-        href="https://www.behance.net/Femrock"
+        href="https://www.behance.net/Femrock" 
+        target="_blank" rel="noopener noreferrer"
         className="behance social"
       >
         <FontAwesomeIcon icon={faBehance} size="2x"/>
       </a>
       <a
-        href="www.linkedin.com/in/mflennon"
+        href="https://www.linkedin.com/in/mflennon"
+        target="_blank" rel="noopener noreferrer"
         className="linkedin social"
       >
         <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
       </a>
       <a
         href="https://github.com/mafda-lennon"
+        target="_blank" rel="noopener noreferrer"
         className="github social"
       >
         <FontAwesomeIcon icon={faGithub} size="2x"/>
