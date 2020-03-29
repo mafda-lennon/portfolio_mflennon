ccde3c4cc9b14c68d4a9f38f21f0f3f01c8e9f47
Femrock <mf.lennon@gmail.com>
2020-03-29 19:32:09 -0300

    favicon & title corrected

--- src/assets/components/Skills.js
+++ src/assets/components/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faCss3, faJs, faReact, faFigma } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5, faCss3, faJs, faNodeJs, faReact, faGit, faGithub, faFigma } from '@fortawesome/free-brands-svg-icons'
 import './SocialIcons.css';
 import './Skills.css';
 
@@ -14,7 +14,10 @@ export default function Skills(){
         <FontAwesomeIcon className="skills" icon={faHtml5} size="2x"/>
         <FontAwesomeIcon className="skills" icon={faCss3} size="2x"/>
         <FontAwesomeIcon className="skills" icon={faJs} size="2x"/>
+        <FontAwesomeIcon className="skills" icon={faNodeJs} size="2x"/>
         <FontAwesomeIcon className="skills" icon={faReact} size="2x"/>
+        <FontAwesomeIcon className="skills" icon={faGit} size="2x"/>
+        <FontAwesomeIcon className="skills" icon={faGithub} size="2x"/>
         <FontAwesomeIcon className="skills" icon={faFigma} size="2x"/>
       </div>
     </div>
